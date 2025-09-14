866ee039b0a7984aba52ca55f6d8c0984b53d2f5
Dennis Obeng-Prempeh <dennisprempeh97@gmail.com>
2025-09-14 08:12:12 +0000

    Script: javaScript is amazing

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+// This script prints a message to the console
+// It uses a constant variable to store the message
+// and logs it without using var
+const myVar = "JavaScript is amazing";
+console.log(myVar);
