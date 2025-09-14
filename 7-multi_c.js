9c7b8ba296e9913577294ce7e52583de80dd7da7
Dennis Obeng-Prempeh <dennisprempeh97@gmail.com>
2025-09-14 09:01:04 +0000

    Correction

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,12 @@
 // Print 'C is fun' x times, where x is the first argument
 const x = Number(process.argv[2]);
-if (isNaN(x) || process.argv[2] === undefined) {
-	console.log("Missing number of occurrences");
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
 } else {
-	let output = "";
-	for (let i = 0; i < x; i++) {
-		output += "C is fun\n";
-	}
-	console.log(output.trim());
+  let output = "";
+  for (let i = 0; i < x; i++) {
+    output += "C is fun\n";
+  }
+  console.log(output);
 }
